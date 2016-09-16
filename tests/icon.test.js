e7096d8c20d1c18826689c4dd540eebee9af5e35
Evan Siroky <evan.siroky@yahoo.com>
2016-09-16 13:37:53 -0700

    test(tests): Add test for Icon component

---
+++ tests/icon.test.js
@@ -0,0 +1,15 @@
+/* global describe, it, expect */
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Icon from '../lib/components/icon'
+
+describe('Icon', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Icon type='pencil' className='test' />
+    ).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
