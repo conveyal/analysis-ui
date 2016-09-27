63f34da78cc1230a3984fe8ecce22d68b53f9650
Evan Siroky <evan.siroky@yahoo.com>
2016-09-26 17:18:47 -0700

    test(modification components): Add testing with experimental mocking technique for two modification

---
+++ __tests__/components/modification/add-trip-pattern.js
@@ -0,0 +1,63 @@
+/* global describe, it, expect, jest */
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import { mockComponent, mockNamedComponents } from '../../../testUtils'
+
+jest.mock('../../../lib/components/buttons', () => { return mockNamedComponents(['Button']) })
+jest.mock('../../../lib/components/icon', () => { return mockComponent('Icon') })
+jest.mock('../../../lib/components/input', () => { return mockNamedComponents(['Checkbox', 'Number']) })
+jest.mock('../../../lib/components/timetable', () => { return mockComponent('Timetable') })
+
+import AddTripPattern from '../../../lib/components/modification/add-trip-pattern'
+
+describe('AddTripPattern', () => {
+  it('renders correctly', () => {
+    const mapState = {
+      allowExtend: true,
+      extendFromEnd: true,
+      followRoad: true,
+      state: 'add-trip-pattern',
+      modificationId: '1234'
+    }
+    const modification = {
+      id: '1234',
+      segments: [],
+      timetables: [
+        {
+          name: 'Test timetable',
+          speed: 40,
+          dwellTime: 10,
+          monday: true,
+          tuesday: true,
+          wednesday: true,
+          thursday: true,
+          friday: true,
+          saturday: false,
+          sunday: false,
+          headwaySecs: 900, // 15 minutes
+          startTime: 28800, // 8am
+          endTime: 57600 // 4pm
+        }
+      ],
+      bidirectional: false
+    }
+    const replaceModificationFn = jest.fn()
+    const setMapStateFn = jest.fn()
+    const updateFn = jest.fn()
+    const tree = renderer.create(
+      <AddTripPattern
+        mapState={mapState}
+        modification={modification}
+        replaceModification={replaceModificationFn}
+        setMapState={setMapStateFn}
+        update={updateFn}
+        />
+    ).toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(replaceModificationFn).not.toBeCalled
+    expect(setMapStateFn).not.toBeCalled
+    expect(updateFn).not.toBeCalled
+  })
+})
