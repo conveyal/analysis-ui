c37d586ef4855b9edc5163c60643afc28db17223
Evan Siroky <evan.siroky@yahoo.com>
2016-09-29 14:13:01 -0700

    test(components): Add tests for SelectProject

---
+++ __tests__/components/select-project.js
@@ -0,0 +1,31 @@
+/* global describe, it, expect, jest */
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import { mockComponents } from '../../testUtils'
+
+jest.mock('react-select', () => 'React-Select')
+jest.mock('../../lib/components/buttons', () => { return mockComponents(['Button']) })
+jest.mock('../../lib/components/input', () => { return mockComponents(['Group']) })
+jest.mock('../../lib/components/panel', () => { return mockComponents(['Body']) })
+
+import SelectProject from '../../lib/components/select-project'
+
+describe('SelectProject', () => {
+  it('renders correctly', () => {
+    const createFn = jest.fn()
+    const mockProjects = [{ id: 1, name: 'P1' }, { id: 2, name: 'P2' }]
+    const pushFn = jest.fn()
+    const tree = renderer.create(
+      <SelectProject
+        create={createFn}
+        projects={mockProjects}
+        push={pushFn}
+        />
+    ).toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(createFn).not.toBeCalled()
+    expect(pushFn).not.toBeCalled()
+  })
+})
