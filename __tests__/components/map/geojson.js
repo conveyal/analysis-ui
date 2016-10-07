3b6e25bc7d7903e02fc3f620355b7b200b3efd28
Evan Siroky <evan.siroky@yahoo.com>
2016-10-07 08:23:15 -0700

    refactor(tests): Refactor geojson test to stub GeoJson dependency

--- __tests__/components/map/geojson.js
+++ __tests__/components/map/geojson.js
@@ -3,37 +3,14 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import GeoJSON from '../../../lib/components/map/geojson'
-
-jest.mock('react-dom')
-jest.genMockFromModule('leaflet')
-const {Map} = jest.genMockFromModule('react-leaflet')
-
-describe('Components > Map > GeoJSON', () => {
-  it('renders correctly', () => {
-    const geojson = {
-      type: 'FeatureCollection',
-      features: []
-    }
-
-    const tree = renderer.create(
-      <Map>
-        <GeoJSON
-          data={geojson}
-          />
-      </Map>
-    ).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
-
-/* Works with Jest 16
 jest.mock('react-leaflet', () => {
   return {
     GeoJson: class mockGeoJson extends React.Component {
       render () {
         return (
-          <mockGeoJson />
+          <mockGeoJson
+            {...this.props}
+            />
         )
       }
     }
@@ -57,4 +34,3 @@ describe('Components > Map > GeoJSON', () => {
     expect(tree).toMatchSnapshot()
   })
 })
-*/
