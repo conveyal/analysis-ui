a8d06bdfa4f41a625e6f3c75baff6d252a9d7561
Evan Siroky <evan.siroky@yahoo.com>
2016-10-07 17:24:37 -0700

    refactor(tests): Refactor test setup for leaflet tests
    This no longer needs the bug fix from mastarm

--- test-utils/jest-setup.js
+++
@@ -1,3 +0,0 @@
-const div = document.createElement('div')
-div.id = 'test'
-document.body.appendChild(div)
--- test-utils/mock-leaflet.js
+++ test-utils/mock-leaflet.js
@@ -3,6 +3,12 @@
 import Leaflet from 'leaflet'
 import uniqueId from 'lodash.uniqueid'
 
+// add a div to jsdom for enzyme to mount to
+const div = document.createElement('div')
+div.id = 'test'
+document.body.appendChild(div)
+
+// implementation for creating leaflet objects
 const makeUniqueLeafletIdFn = (prefix) => {
   return () => {
     return {
@@ -11,6 +17,8 @@ const makeUniqueLeafletIdFn = (prefix) => {
   }
 }
 
+// mock these things w/ jest so that it can be verified that
+// elements are to be created by react-leaflet
 Leaflet.geoJson = jest.fn(makeUniqueLeafletIdFn('geoJson'))
 Leaflet.marker = jest.fn(makeUniqueLeafletIdFn('marker'))
 
