d7331caacb2fbe9b77ab75c38ad48ba68ed93ff2
Evan Siroky <evan.siroky@yahoo.com>
2016-10-10 16:54:10 -0700

    test(components): Add tests for rest of components in main components directory

---
+++ __tests__/components/import-shapefile.js
@@ -0,0 +1,33 @@
+/* global describe, it, expect, jest */
+
+import { mount } from 'enzyme'
+import { mountToJson } from 'enzyme-to-json'
+import React from 'react'
+
+import ImportShapefile from '../../lib/components/import-shapefile'
+
+describe('Component > ImportShapefile', () => {
+  it('renders correctly', () => {
+    const props = {
+      close: jest.fn(),
+      setModification: jest.fn(),
+      variants: [],
+      scenarioId: '1'
+    }
+
+    // mount component
+    const tree = mount(
+      <ImportShapefile
+        {...props}
+        />
+    )
+    expect(mountToJson(tree)).toMatchSnapshot()
+    const noCalls = [
+      'close',
+      'setModification'
+    ]
+    noCalls.forEach((fn) => {
+      expect(props[fn]).not.toBeCalled()
+    })
+  })
+})
