7a66c8eee993d2d72a5458658a7b6b950168b8f5
Evan Siroky <evan.siroky@yahoo.com>
2016-10-17 22:28:25 -0700

    test(reducers): Add tests for reducers
    Also refactor reducers so that handlers are sorted alphabetically

---
+++ __tests__/reducers/analysis.js
@@ -0,0 +1,40 @@
+/* globals describe, expect, it */
+
+import { handleActions } from 'redux-actions'
+
+import * as analysis from '../../lib/reducers/analysis'
+
+describe('reducers > analysis', () => {
+  const reducer = handleActions(analysis.reducers, analysis.initialState)
+
+  // Default State Test
+  it('should handle default state', () => {
+    expect(reducer(undefined, { type: 'blah', payload: {} })).toMatchSnapshot()
+  })
+
+  // Specific Handler Tests
+  it('should handle clear isochrone results', () => {
+    const action = { type: 'clear isochrone results', payload: {} }
+    expect(reducer(undefined, action)).toMatchSnapshot()
+  })
+
+  it('should handle set isochrone cutoff', () => {
+    const action = { type: 'set isochrone cutoff', payload: 123 }
+    expect(reducer(undefined, action)).toMatchSnapshot()
+  })
+
+  it('should handle set isochrone fetch status', () => {
+    const action = { type: 'set isochrone fetch status', payload: true }
+    expect(reducer(undefined, action)).toMatchSnapshot()
+  })
+
+  it('should handle set isochrone latlng', () => {
+    const action = { type: 'set isochrone latlng', payload: 123 }
+    expect(reducer(undefined, action)).toMatchSnapshot()
+  })
+
+  it('should handle set isochrone results', () => {
+    const action = { type: 'set isochrone results', payload: { isochrone: 123 } }
+    expect(reducer(undefined, action)).toMatchSnapshot()
+  })
+})
