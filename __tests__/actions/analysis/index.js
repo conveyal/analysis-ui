cdbb9f82419c2318a14fd582df7209d4bab95b6c
Evan Siroky <evan.siroky@yahoo.com>
2016-10-18 20:25:36 -0700

    test(actions): Add some tests for actions
    Also, alphabetize actions in actions code

---
+++ __tests__/actions/analysis/index.js
@@ -0,0 +1,63 @@
+/* global describe, expect, it */
+
+// import { setGrids } from '../../../lib/utils/browsochrones'
+
+import * as analysis from '../../../lib/actions/analysis'
+
+describe('actions > analysis', () => {
+  it('clearIsochroneResults should work', () => {
+    expect(analysis.clearIsochroneResults()).toMatchSnapshot()
+  })
+
+  it('setIsochroneCutoff should work', () => {
+    expect(analysis.setIsochroneCutoff()).toMatchSnapshot()
+  })
+
+  it('setIsochroneFetchStatus should work', () => {
+    expect(analysis.setIsochroneFetchStatus()).toMatchSnapshot()
+  })
+
+  it('setIsochroneLatLng should work', () => {
+    expect(analysis.setIsochroneLatLng({ lat: 12, lon: 34 })).toMatchSnapshot()
+  })
+
+  it('setIsochroneResults should work', () => {
+    expect(analysis.setIsochroneResults()).toMatchSnapshot()
+  })
+
+  it('fetchIsochrone should resolve error', (done) => {
+    const actionArgs = {
+      scenarioId: '1',
+      bundleId: '1',
+      modifications: [],
+      isochroneCutoff: 1,
+      origin: 1
+    }
+    const actionResults = analysis.fetchIsochrone(actionArgs)
+    actionResults[2].then((result) => {
+      expect(result).toMatchSnapshot()
+      done()
+    })
+  })
+
+  /* Not sure how this thing works
+  it('fetchIsochrone should work', (done) => {
+    const actionArgs = {
+      scenarioId: '1',
+      bundleId: '1',
+      modifications: [],
+      isochroneCutoff: 1,
+      origin: 1
+    }
+    setGrids([])
+    const actionResults = analysis.fetchIsochrone(actionArgs)
+    expect(actionResults.slice(0, 2)).toMatchSnapshot()
+    console.log(actionResults[2])
+    actionResults[2].then((result) => {
+      console.log('resolved')
+      expect(result).toMatchSnapshot()
+      done()
+    })
+  })
+  */
+})
---
+++ __tests__/actions/map.js
@@ -0,0 +1,21 @@
+/* global describe, expect, it */
+
+import * as map from '../../lib/actions/map'
+
+describe('actions > map', () => {
+  it('addComponent should work', () => {
+    expect(map.addComponent()).toMatchSnapshot()
+  })
+
+  it('removeComponent should work', () => {
+    expect(map.removeComponent()).toMatchSnapshot()
+  })
+
+  it('setCenter should work', () => {
+    expect(map.setCenter({ lat: 12, lon: 34 })).toMatchSnapshot()
+  })
+
+  it('setZoom should work', () => {
+    expect(map.setZoom()).toMatchSnapshot()
+  })
+})
