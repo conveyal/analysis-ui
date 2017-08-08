48ec468d2a6d42dbc21311aa00beede16b8c8c54
Trevor Gerhardt <trevorgerhardt@gmail.com>
2017-08-08 15:39:30 +0100

    style(format): Format all JS with prettier

--- __mocks__/isomorphic-fetch.js
+++ __mocks__/isomorphic-fetch.js
@@ -1,5 +1,4 @@
 const ifetch = require('isomorphic-fetch')
 
-module.exports = global.fetch = (url, opts) => url[0] === '/'
-  ? ifetch(`http://mockhost.com${url}`, opts)
-  : ifetch(url, opts)
+module.exports = global.fetch = (url, opts) =>
+  url[0] === '/' ? ifetch(`http://mockhost.com${url}`, opts) : ifetch(url, opts)
