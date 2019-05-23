d3036b13834316f3305740a8d8808ac3921ffbb1
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-05-23 13:38:45 +0000

    test(components): Fix component tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
+// Set the `API_URL`
+process.env.API_URL = '/api'
+
 module.exports = {
   moduleNameMapper: {
-    '\\.css$': '<rootDir>/__mocks__/css-import.js'
+    '\\.css$': '<rootDir>/__mocks__/css-import.js',
+    '\\.png$': '<rootDir>/__mocks__/png-import.js'
   },
   // Add the current working directory to the path to allow absolute imports from /lib
   modulePaths: ['.'],
+  notify: true,
   setupFiles: ['./lib/utils/enzyme'],
   snapshotSerializers: [
     './lib/utils/geojson-snapshot-serializer',
