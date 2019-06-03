3639f949bb1b2bd66a9f240b086ac90cf6267e74
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-06-03 12:42:00 +0800

    refactor(api): Proxy api for local dev and deployment

---
+++ api/proxy/index.js
@@ -0,0 +1,9 @@
+const httpProxy = require('http-proxy')
+const proxy = httpProxy.createProxyServer({
+  secure: false, // TODO true in production?
+  target: process.env.API_URL
+})
+
+module.exports = function(req, res) {
+  proxy.web(req, res)
+}
--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withImages(
       url: false // enable importing CSS from node_modules
     },
     env: {
-      API_URL: process.env.API_URL,
+      API_URL: process.env.API_URL || '/api',
       AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
       AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
       GRAPHHOPPER_API_KEY: process.env.GRAPHHOPPER_API_KEY,
