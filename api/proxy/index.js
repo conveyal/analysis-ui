c38db82405a9b475d02e8ef01939a89b0862eea4
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-06-10 10:05:39 +0800

    Log api url

--- api/proxy/index.js
+++ api/proxy/index.js
@@ -1,6 +1,8 @@
 const httpProxy = require('http-proxy')
 const {timer} = require('../../lib/utils/metric')
 
+console.log('api/proxy', process.env.API_URL)
+
 const proxy = httpProxy.createProxyServer({
   secure: false, // TODO true in production?
   target: process.env.API_URL
--- next.config.js
+++ next.config.js
@@ -6,15 +6,20 @@ const path = require('path')
 // Pull in .env
 require('dotenv').config()
 
+const env = {
+  API_URL: process.env.API_URL,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  GRAPHHOPPER_API_KEY: process.env.GRAPHHOPPER_API_KEY,
+  MAPTILER_KEY: process.env.MAPTILER_KEY
+}
+
+// Log env for sanity
+console.log(env)
+
 module.exports = withBundleAnalyzer({
   target: 'serverless',
-  env: {
-    API_URL: process.env.API_URL,
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    GRAPHHOPPER_API_KEY: process.env.GRAPHHOPPER_API_KEY,
-    MAPTILER_KEY: process.env.MAPTILER_KEY
-  },
+  env,
   webpack: config => {
     // Allow `import 'lib/message'`
     config.resolve.alias['lib'] = path.join(__dirname, 'lib')
