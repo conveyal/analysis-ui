bd7298838a8caf4b57cf734ea57023ade39435eb
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-06-11 22:06:42 +0800

    Dim pages when loading

--- pages/modifications.js
+++ pages/modifications.js
@@ -1,11 +1,27 @@
+import React from 'react'
+
 import {loadProjectAndModifications} from 'lib/actions/project'
-import Modifications from 'lib/components/modification/list'
+import List from 'lib/components/modification/list'
+
+import SelectProject from './projects'
+
+/**
+ * Show Select Project if a project has not been selected
+ */
+function Modifications(p) {
+  if (!p.projectId) {
+    return <SelectProject {...p} />
+  } else {
+    return <List {...p} />
+  }
+}
 
 /**
  * Populates props with bundle, feeds, modifications, and project.
  */
 Modifications.getInitialProps = async ctx => {
   const {projectId} = ctx.query
+  if (!projectId) return await SelectProject.getInitialProps(ctx)
   return await ctx.reduxStore.dispatch(loadProjectAndModifications(projectId))
 }
 
