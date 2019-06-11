ed1b40018c411934ac4fbb2cb1b43121909b143e
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-06-11 16:24:34 +0800

    Fix aggregation area selection

--- pages/regional-analysis.js
+++ pages/regional-analysis.js
@@ -1,12 +1,17 @@
+import {loadRegion} from 'lib/actions/region'
 import {load} from 'lib/actions/analysis/regional'
 import RegionalAnalysis from 'lib/containers/regional-analysis-results'
 
 RegionalAnalysis.getInitialProps = async ctx => {
   const {analysisId, regionId} = ctx.query
-  const regionalAnalyses = await ctx.reduxStore.dispatch(load(regionId))
+  const [regionalAnalyses, region] = await Promise.all([
+    ctx.reduxStore.dispatch(load(regionId)),
+    ctx.reduxStore.dispatch(loadRegion(regionId))
+  ])
   return {
     analysis: regionalAnalyses.find(a => a._id === analysisId),
-    regionalAnalyses
+    regionalAnalyses,
+    region
   }
 }
 
