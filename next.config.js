188efc9675e5ae4b1a191c52e6c5bd276e68abb5
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-06-13 14:11:18 +0800

    Remove graphhopper, maptiler (for now)

--- next.config.js
+++ next.config.js
@@ -10,8 +10,7 @@ const env = {
   API_URL: process.env.API_URL,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  GRAPHHOPPER_API_KEY: process.env.GRAPHHOPPER_API_KEY,
-  MAPTILER_KEY: process.env.MAPTILER_KEY
+  MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN
 }
 
 // Log env for sanity
