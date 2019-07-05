6a6fcf03140c726aefaeaed5bc47d74fa586b280
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-07-05 16:34:43 +0800

    Fix sidebar url matching

--- pages/regions/[regionId]/projects/[projectId]/index.js
+++ pages/regions/[regionId]/projects/[projectId]/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 import {loadProjectAndModifications} from 'lib/actions/project'
+import {load} from 'lib/actions/region'
 import List from 'lib/components/modification/list'
+import SelectProject from 'lib/components/select-project'
 import withFetch from 'lib/with-fetch'
 
-import SelectProject, {fetchData as selectProjectFetchData} from './projects'
+const noProjectId = pid => !pid || pid === 'undefined'
 
 /**
  * Show Select Project if a project has not been selected
  */
 function Modifications(p) {
-  if (!p.query.projectId) {
+  if (noProjectId(p.query.projectId)) {
     return <SelectProject {...p} />
   } else {
     return <List {...p} />
@@ -21,8 +23,8 @@ function Modifications(p) {
  * Populates props with bundle, feeds, modifications, and project.
  */
 function fetchData(dispatch, query) {
-  const {projectId} = query
-  if (!projectId) return selectProjectFetchData(dispatch, query)
+  const {projectId, regionId} = query
+  if (noProjectId(projectId)) return dispatch(load(regionId))
   return dispatch(loadProjectAndModifications(projectId))
 }
 
