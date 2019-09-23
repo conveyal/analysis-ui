b4add90f0cdefe6fa4a2d2d2b0fab9c0526c195c
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-09-23 13:25:36 +0800

    Remove console.log

--- pages/regions/[regionId]/index.js
+++ pages/regions/[regionId]/index.js
@@ -3,7 +3,6 @@ import SelectProject from 'lib/components/select-project'
 import withInitialFetch from 'lib/with-initial-fetch'
 
 export function initialFetch(store, query) {
-  console.log('iniitlafetching')
   return store.dispatch(load(query.regionId))
 }
 
