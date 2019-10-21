9722de6bcba98512de759bf6fa8c5a96165be5b2
Trevor Gerhardt <trevorgerhardt@gmail.com>
2019-10-21 21:30:32 +0800

    Handle resource upload/download

--- pages/regions/[regionId]/resources/index.js
+++ pages/regions/[regionId]/resources/index.js
@@ -1,17 +1,36 @@
-import {useRouter} from 'next/router'
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 
-import {ButtonLink} from 'lib/components/buttons'
+import {
+  deleteResource,
+  downloadResource,
+  loadAllResources
+} from 'lib/actions/resources'
+import {Button, ButtonLink} from 'lib/components/buttons'
 import InnerDock from 'lib/components/inner-dock'
 import Select from 'lib/components/select'
+import downloadData from 'lib/utils/download-data'
 import withInitialFetch from 'lib/with-initial-fetch'
 
 function Resources(p) {
-  const router = useRouter()
-  const resources = useSelector(state => state.resources)
+  const [activeResource, setActiveResource] = React.useState()
+  const dispatch = useDispatch()
 
-  function selectResource(resource) {}
+  function _download() {
+    dispatch(downloadResource(activeResource)).then(value => {
+      if (activeResource.offline) {
+        downloadData(value, activeResource.filename, activeResource.type)
+      } else {
+        window.open(value)
+      }
+    })
+  }
+
+  function _delete() {
+    if (window.confirm('Are you sure you want to delete this resource?')) {
+      dispatch(deleteResource(activeResource))
+    }
+  }
 
   return (
     <InnerDock className='block'>
@@ -26,18 +45,30 @@ function Resources(p) {
       </ButtonLink>
       <br />
       <Select
-        getOptionLabel={r => `${r.contentType} -- ${r.name}`}
+        getOptionLabel={r => `${r.name}: ${r.contentType}`}
         getOptionValue={r => r._id}
-        onChange={selectResource}
-        options={resources}
+        onChange={resource => setActiveResource(resource)}
+        options={p.resources}
       />
+      <br />
+      {activeResource && (
+        <>
+          <h6>Name: {activeResource.name}</h6>
+          <Button block style='success' onClick={_download}>
+            Download
+          </Button>
+          <Button block style='danger' onClick={_delete}>
+            Delete
+          </Button>
+        </>
+      )}
     </InnerDock>
   )
 }
 
-async function initialFetch() {
+async function initialFetch(store, query) {
   return {
-    resources: []
+    resources: await store.dispatch(loadAllResources(query))
   }
 }
 
--- pages/regions/[regionId]/resources/upload.js
+++ pages/regions/[regionId]/resources/upload.js
@@ -1,16 +1,22 @@
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 import {useDispatch} from 'react-redux'
 
 import {createResource} from 'lib/actions/resources'
+import Alert from 'lib/components/alert'
 import {Button} from 'lib/components/buttons'
+import Icon from 'lib/components/icon'
 import {File, Text} from 'lib/components/input'
 import InnerDock from 'lib/components/inner-dock'
+import Link from 'lib/components/link'
 import getInitialAuth from 'lib/get-initial-auth'
+
+const EXTS = ['.json'] // later: csv, pbf, zip
 
 export default function UploadResource(p) {
   const dispatch = useDispatch()
   const [status, setStatus] = React.useState()
-  const [errors, setErrors] = React.useState()
+  const [error, setError] = React.useState()
   const [file, setFile] = React.useState()
   const [name, setName] = React.useState('')
   const [uploading, setUploading] = React.useState(false)
@@ -20,12 +26,13 @@ export default function UploadResource(p) {
     setUploading(true)
     dispatch(createResource({name, file, regionId: p.query.regionId}))
       .then(() => {
-        setErrors()
-        setStatus('Finished uploading resource...')
+        setError()
+        setStatus('Finished uploading resource!')
       })
       .catch(e => {
+        console.error(e)
         setStatus()
-        setErrors(e)
+        setError(e.message)
       })
       .finally(() => {
         setUploading(false)
@@ -34,13 +41,22 @@ export default function UploadResource(p) {
 
   return (
     <InnerDock className='block'>
-      <legend>Upload Resource</legend>
-      <p>Accepts `.json` and `.geojson` files.</p>
-      {errors && <div className='alert alert-danger'>{errors}</div>}
-      {status && <div className='alert alert-info'>{status}</div>}
+      <legend>
+        <Link to='resources' {...p.query}>
+          <a>
+            <Icon icon={faChevronLeft} />
+          </a>
+        </Link>
+        <span>Upload Resource</span>
+      </legend>
+      <p>
+        Accepts <code>{EXTS.join(',')}</code> files.
+      </p>
+      <Alert style='danger' text={error} />
+      <Alert text={status} onClear={() => setStatus()} />
       <Text label='Name' onChange={e => setName(e.currentTarget.value)} />
       <File
-        accept='.json,.geojson'
+        accept={EXTS.join(',')}
         label='Select file'
         onChange={e => setFile(e.target.files[0])}
       />
