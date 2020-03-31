2b48c128daadac932ddf47e0154acaca125456ab
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-03-31 18:54:09 +0800

    Select existing feeds and OSM
    During bundle creation

--- pages/regions/[regionId]/bundles/create.js
+++ pages/regions/[regionId]/bundles/create.js
@@ -1,3 +1,13 @@
-import CreateBundle from 'lib/components/create-bundle'
+import React from 'react'
 
-export default CreateBundle
+import {loadBundles} from 'lib/actions'
+import CreateBundle from 'lib/components/create-bundle'
+import withInitialFetch from 'lib/with-initial-fetch'
+
+const CreateBundlePage = p => <CreateBundle regionId={p.query.regionId} />
+
+function initialFetch(store, query) {
+  return store.dispatch(loadBundles({regionId: query.regionId}))
+}
+
+export default withInitialFetch(CreateBundlePage, initialFetch)
