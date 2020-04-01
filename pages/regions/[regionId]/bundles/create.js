4dd212bbd7df8dbe27c8dc3654620311abb9d57d
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-04-01 20:27:13 +0800

    Restyle bundle creation page

--- pages/regions/[regionId]/bundles/create.js
+++ pages/regions/[regionId]/bundles/create.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 import {loadBundles} from 'lib/actions'
+import {loadRegion} from 'lib/actions/region'
 import CreateBundle from 'lib/components/create-bundle'
 import withInitialFetch from 'lib/with-initial-fetch'
 
 const CreateBundlePage = p => <CreateBundle regionId={p.query.regionId} />
 
 function initialFetch(store, query) {
-  return store.dispatch(loadBundles({regionId: query.regionId}))
+  return Promise.all([
+    store.dispatch(loadBundles({regionId: query.regionId})),
+    store.dispatch(loadRegion(query.regionId))
+  ])
 }
 
 export default withInitialFetch(CreateBundlePage, initialFetch)
