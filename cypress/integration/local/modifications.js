f9e2044c6888d3eb45c5c6b22bdcfe6b350a8254
Nate Wessel <bike756@gmail.com>
2020-04-20 22:52:44 -0400

    remove redundant test

--- cypress/integration/local/modifications.js
+++ cypress/integration/local/modifications.js
@@ -35,6 +35,7 @@ describe('Modifications', () => {
     // go back and see if it saved
     cy.findByTitle(/Edit Modifications/).click({force: true})
     cy.location('pathname').should('match', /\/projects\/.{24}$/)
+    // TODO this needs to be conditional if the list of mods is very full
     cy.contains(modType).parent().contains(modName).click()
     cy.location('pathname').should('match', /.*\/modifications\/.{24}$/)
     cy.contains(modName)
@@ -44,25 +45,6 @@ describe('Modifications', () => {
     cy.location('pathname').should('match', /.*\/projects\/.{24}$/)
     cy.contains('Create a modification')
     cy.findByText(modName).should('not.exist')
-  })
-
-  it('saves Add Trip Pattern mod', () => {
-    let modName = 'Add Trip ' + Date.now()
-    cy.findByRole('link', {name: 'Create a modification'}).click()
-    cy.findByLabelText(/Modification type/i).select('Add Trip Pattern')
-    cy.findByLabelText(/Modification name/i).type(modName)
-    cy.findByRole('link', {name: 'Create'}).click()
-    cy.location('pathname').should('match', /.*\/modifications\/.{24}$/)
-    // now editing the modification
-    cy.contains(modName)
-    cy.findByRole('link', {name: /Add description/}).click()
-    cy.findByLabelText('Description').type('the description')
-    // go back and see if this saved
-    cy.findByTitle(/Edit Modifications/).click({force: true})
-    cy.location('pathname').should('match', /\/projects\/.{24}$/)
-    cy.contains(/Add Trip Pattern/)
-      .parent()
-      .contains(modName)
   })
 
   it('can be imported from shapefile, in theory', () => {
