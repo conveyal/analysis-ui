003e64509e779b2edea120e2ad63c99afcf07f9b
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-05-27 09:46:49 +0800

    Initial change

--- pages/regions/[regionId]/analysis.js
+++ pages/regions/[regionId]/analysis.js
@@ -6,11 +6,14 @@ import {
 } from 'lib/actions/analysis'
 import SinglePointAnalysis from 'lib/containers/single-point-analysis'
 import MapLayout from 'lib/layouts/map'
+import {setActiveOpportunityDataset} from 'lib/modules/opportunity-datasets/actions'
 import withInitialFetch from 'lib/with-initial-fetch'
 
 const AnalysisPage = withInitialFetch(
   SinglePointAnalysis,
   (dispatch, query) => {
+    // Set the active id
+    dispatch(setActiveOpportunityDataset(query.opportunityDatasetId))
     dispatch(setScenarioApplicationWarnings(null))
     dispatch(setScenarioApplicationErrors(null))
 
