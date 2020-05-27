0e8d53cc88e4f2bdbe5342ea16eecbab2ff8fb2f
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-05-27 11:12:42 +0800

    Refactor analysis page, remove container

--- pages/regions/[regionId]/analysis.js
+++ pages/regions/[regionId]/analysis.js
@@ -1,26 +1,40 @@
+import dynamic from 'next/dynamic'
+
 import {load} from 'lib/actions/region'
 import {loadBookmarks} from 'lib/actions/bookmark'
 import {
   setScenarioApplicationErrors,
   setScenarioApplicationWarnings
 } from 'lib/actions/analysis'
-import SinglePointAnalysis from 'lib/containers/single-point-analysis'
+import {load as loadAllRegionalAnalyses} from 'lib/actions/analysis/regional'
 import MapLayout from 'lib/layouts/map'
 import {setActiveOpportunityDataset} from 'lib/modules/opportunity-datasets/actions'
 import withInitialFetch from 'lib/with-initial-fetch'
 
+// Loads many map components
+const SinglePointAnalysis = dynamic(() => import('lib/components/analysis'), {
+  ssr: false
+})
+
 const AnalysisPage = withInitialFetch(
   SinglePointAnalysis,
-  (dispatch, query) => {
+  async (dispatch, query) => {
     // Set the active id
     dispatch(setActiveOpportunityDataset(query.opportunityDatasetId))
     dispatch(setScenarioApplicationWarnings(null))
     dispatch(setScenarioApplicationErrors(null))
 
-    return Promise.all([
+    const [totalRegion, bookmarks, regionalAnalyses] = await Promise.all([
       dispatch(load(query.regionId)),
-      dispatch(loadBookmarks(query.regionId))
+      dispatch(loadBookmarks(query.regionId)),
+      dispatch(loadAllRegionalAnalyses(query.regionId))
     ])
+
+    return {
+      bookmarks,
+      regionalAnalyses,
+      ...totalRegion
+    }
   }
 )
 
