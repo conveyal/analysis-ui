2104452edf50f7a5afd13d8b4421af28d731c13d
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-05-31 11:36:40 +0800

    Fix stray bracket

--- lib/components/analysis/index.tsx
+++ lib/components/analysis/index.tsx
@@ -457,7 +457,6 @@ function RequestSettings({
               >
                 {project.name}
               </Heading>
-              }
               <Heading
                 size='sm'
                 color='gray.500'
