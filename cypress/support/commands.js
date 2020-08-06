6a233bb45a65bbdb434d607c5185462a34fe849c
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-08-06 10:58:58 +0800

    Fix bundle selection

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -174,10 +174,7 @@ function createNewBundle() {
   cy.findByText(/Processing/, {timeout: 30000}).should('not.exist')
   // go back and grab the UUID
   cy.navTo('Network Bundles')
-  cy.findByText(/Select.../)
-    .parent()
-    .click()
-    .type(bundleName + '{enter}')
+  cy.findByLabelText(/or select an existing one/).type(bundleName + '{enter}')
   return cy
     .location('pathname')
     .should('match', /bundles\/\w{24}$/)
