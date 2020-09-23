3b014de4adf1afbd8c710c27d35407530c2a0055
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-09-23 20:47:34 +0800

    Ensure cleared projects show results as stale

--- lib/components/map/geojson.tsx
+++ lib/components/map/geojson.tsx
@@ -3,10 +3,10 @@ import {GeoJSON, GeoJSONProps} from 'react-leaflet'
 import {v4 as uuid} from 'uuid'
 
 /**
- * Increment the render count when the data changes to replace the entire underlying GeoJSON component.
+ * Increment the render count when the data or style changes to replace the entire underlying GeoJSON component.
  */
-export default function KeyedGeoJSON({data, ...p}: GeoJSONProps) {
+export default function KeyedGeoJSON({data, style, ...p}: GeoJSONProps) {
   const [key, setKey] = useState(() => uuid())
-  useEffect(() => setKey(uuid()), [data])
-  return <GeoJSON data={data || []} key={key} {...p} />
+  useEffect(() => setKey(uuid()), [data, style])
+  return <GeoJSON data={data || []} key={key} style={style} {...p} />
 }
