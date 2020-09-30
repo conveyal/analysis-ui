fa397632cda3f3453b66857bd06864b8f563e763
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-09-30 22:53:40 +0800

    Default tooltip font size to medium
    Apparently it's hard for me to read past 10pm. So it may be hard for others. Defaulting to medium (vs small)

--- lib/components/tip.tsx
+++ lib/components/tip.tsx
@@ -1,14 +1,24 @@
-import {Tooltip} from '@chakra-ui/core'
+import {Tooltip, TooltipProps} from '@chakra-ui/core'
 
 const zIndex = 1500 // Chakra modals are set to 1400
 
+type TipProps = Omit<TooltipProps, 'aria-label'> & {
+  isDisabled?: boolean
+}
+
 // Common Tooltip defaults used acrossed the application
-export default function Tip({children, isDisabled = false, label, ...p}) {
-  if (isDisabled) return children
+export default function Tip({
+  children,
+  isDisabled = false,
+  label,
+  ...p
+}: TipProps) {
+  if (isDisabled) return <>{children}</>
 
   return (
     <Tooltip
       aria-label={`Tooltip: ${label}`}
+      fontSize='md'
       hasArrow
       label={label}
       zIndex={zIndex}
