9f2ee3c888658ee04fb5e34cdceb88d54893dbcf
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-10-01 22:17:53 +0800

    Make docs links blue

--- lib/components/docs-link.tsx
+++ lib/components/docs-link.tsx
@@ -5,11 +5,17 @@ import Icon from './icon'
 import Tip from './tip'
 
 const docsBaseURL = 'https://docs.conveyal.com'
+const _hover = {color: 'blue.700'}
 
 export default function DocsLink({to}) {
   return (
-    <Tip label='Learn more'>
-      <Link href={`${docsBaseURL}/${to}`} isExternal>
+    <Tip label='View docs to learn more'>
+      <Link
+        color='blue.500'
+        href={`${docsBaseURL}/${to}`}
+        isExternal
+        _hover={_hover}
+      >
         <Icon icon={faQuestionCircle} />
       </Link>
     </Tip>
