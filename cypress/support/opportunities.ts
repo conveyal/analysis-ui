31ba72019d2d86a0863a14b4cdc7ea5fe44cff09
Trevor Gerhardt <trevorgerhardt@gmail.com>
2021-01-05 16:47:08 +0800

    Rearrange regional analysis tests
    And add a test that runs an analysis with small bounds. There is currently an error when that happens, so it's skipped by default.

--- cypress/support/opportunities.ts
+++ cypress/support/opportunities.ts
@@ -13,6 +13,12 @@ Cypress.Commands.add(
       filePath,
       encoding: 'base64'
     })
+
+    if (filePath.endsWith('csv')) {
+      cy.findByLabelText(/Latitude/).type('lat')
+      cy.findByLabelText(/Longitude/).type('lon')
+    }
+
     cy.findButton(/Upload a new opportunity dataset/).click()
     cy.navComplete()
     // find the message showing this upload is complete
@@ -21,7 +27,7 @@ Cypress.Commands.add(
       .parent()
       .as('notice')
     // check number of fields uploaded
-    cy.get('@notice').contains(/Finished uploading 1 feature/i)
+    cy.get('@notice').contains(/Finished uploading \d feature/i)
     // close the message
     cy.get('@notice').findByRole('button', {name: /Close/}).click()
     // now grab the ID
