b1c66c44241752a3b7a98588d0a206e5f640386b
ansons <ansonstewart@gmail.com>
2021-02-09 16:23:32 -0500

    Fix cypress test (features → layers)

--- cypress/integration/2-opportunities.ts
+++ cypress/integration/2-opportunities.ts
@@ -66,7 +66,7 @@ describe('Opportunity Datasets', function () {
         .as('notice')
       // check number of fields uploaded
       cy.get('@notice').contains(
-        `Finished uploading ${expectedFieldCount} features`
+        `Finished uploading ${expectedFieldCount} layers`
       )
       // close the message
       cy.get('@notice').findByRole('button', {name: /Close/}).click()
