eefab9163f8e965db81c2e11213bc1f2460be6a1
Trevor Gerhardt <trevorgerhardt@gmail.com>
2021-02-18 16:52:53 -0600

    Check for total origins in create modal

--- cypress/integration/regional/basic.ts
+++ cypress/integration/regional/basic.ts
@@ -2,65 +2,91 @@ import {getDefaultRegion, scratchRegion} from '../utils'
 
 describe('Regional', () => {
   const region = getDefaultRegion()
-  const regionalAnalysis = region.getRegionalAnalysis('Basic Regional Test', {
-    settings: {
-      bounds: scratchRegion.customRegionSubset
-    }
-  })
 
-  // For faster testing results, comment out the below
-  after(() => regionalAnalysis.delete())
-
-  beforeEach(() => {
-    cy.findByText(/Access to/i)
-      .parent()
-      .as('legend')
-  })
-
-  it('verifies regional analysis results', function () {
-    cy.get('@legend').should('not.contain', 'Loading grids')
-    // compare to self with different time cutoff and check the legend again
-    cy.findByLabelText(/Compare to/).type(`${regionalAnalysis.name}{enter}`, {
-      force: true
+  describe('Create Modal', () => {
+    before(() => {
+      region.initializeAnalysisDefaults()
+      region.fetchAccessibilityComparison(region.center)
+      cy.getPrimaryAnalysisSettings().within(() => {
+        cy.findButton(/Regional analysis/).click()
+      })
     })
-    // TODO make these select elements easier to identify
-    cy.findByText(/Compare to/)
-      .parent()
-      .parent()
-      .findByRole('option', {name: '45 minutes'})
-      .parent()
-      .select('60 minutes')
-    cy.get('@legend').should('not.contain', 'Loading grids')
-    // test aggreation area upload
-    cy.findByText(/upload new aggregation area/i).click()
-    cy.findByRole('button', {name: 'Upload'}).as('upload').should('be.disabled')
-    cy.findByLabelText(/Aggregation area name/i).type('cities')
-    cy.findByLabelText(/Select aggregation area files/i)
-      .attachFile({
-        filePath: scratchRegion.aggregationAreas.files[0],
-        encoding: 'base64'
+
+    it('should show total origins', () => {
+      cy.findByText(/Analysis will run for 11,644 origin points/)
+    })
+
+    it(
+      'should show error if opportunity datasets selected are not the same size'
+    )
+    it(
+      'should show error if a freeform pointset and other opportunity datasets are selected'
+    )
+  })
+
+  describe('Results', () => {
+    const regionalAnalysis = region.getRegionalAnalysis('Basic Regional Test', {
+      settings: {
+        bounds: scratchRegion.customRegionSubset
+      }
+    })
+
+    // For faster testing results, comment out the below
+    after(() => regionalAnalysis.delete())
+
+    beforeEach(() => {
+      cy.findByText(/Access to/i)
+        .parent()
+        .as('legend')
+    })
+
+    it('verifies regional analysis results', function () {
+      cy.get('@legend').should('not.contain', 'Loading grids')
+      // compare to self with different time cutoff and check the legend again
+      cy.findByLabelText(/Compare to/).type(`${regionalAnalysis.name}{enter}`, {
+        force: true
       })
-      .attachFile({
-        filePath: scratchRegion.aggregationAreas.files[1],
-        encoding: 'base64'
-      })
-      .attachFile({
-        filePath: scratchRegion.aggregationAreas.files[2],
-        encoding: 'base64'
-      })
-      .attachFile({
-        filePath: scratchRegion.aggregationAreas.files[3],
-        encoding: 'base64'
-      })
-    cy.findByLabelText(/Union/).uncheck({force: true})
-    cy.findByLabelText(/Shapefile attribute to use as aggregation area name/i)
-      .clear()
-      .type(scratchRegion.aggregationAreas.nameField)
-    cy.get('@upload').scrollIntoView().click()
-    cy.contains(/Upload complete/, {timeout: 30000}).should('be.visible')
-    // TODO label dissociated from input
-    //cy.findByLabelText(/Aggregate results to/i)
-    //  .type(this.region.aggregationAreas.sampleName+'{enter}')
+      // TODO make these select elements easier to identify
+      cy.findByText(/Compare to/)
+        .parent()
+        .parent()
+        .findByRole('option', {name: '45 minutes'})
+        .parent()
+        .select('60 minutes')
+      cy.get('@legend').should('not.contain', 'Loading grids')
+      // test aggreation area upload
+      cy.findByText(/upload new aggregation area/i).click()
+      cy.findByRole('button', {name: 'Upload'})
+        .as('upload')
+        .should('be.disabled')
+      cy.findByLabelText(/Aggregation area name/i).type('cities')
+      cy.findByLabelText(/Select aggregation area files/i)
+        .attachFile({
+          filePath: scratchRegion.aggregationAreas.files[0],
+          encoding: 'base64'
+        })
+        .attachFile({
+          filePath: scratchRegion.aggregationAreas.files[1],
+          encoding: 'base64'
+        })
+        .attachFile({
+          filePath: scratchRegion.aggregationAreas.files[2],
+          encoding: 'base64'
+        })
+        .attachFile({
+          filePath: scratchRegion.aggregationAreas.files[3],
+          encoding: 'base64'
+        })
+      cy.findByLabelText(/Union/).uncheck({force: true})
+      cy.findByLabelText(/Shapefile attribute to use as aggregation area name/i)
+        .clear()
+        .type(scratchRegion.aggregationAreas.nameField)
+      cy.get('@upload').scrollIntoView().click()
+      cy.contains(/Upload complete/, {timeout: 30000}).should('be.visible')
+      // TODO label dissociated from input
+      //cy.findByLabelText(/Aggregate results to/i)
+      //  .type(this.region.aggregationAreas.sampleName+'{enter}')
+    })
   })
 
   it('test starting and deleting a running analysis')
